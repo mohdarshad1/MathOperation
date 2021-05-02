c20f38fb86d44fb6bdae033d014327995c4cc74a
mohdarshad1 <mdarshad181@gmail.com>
2021-05-02 18:09:01 +0530

    [Mohd Arshad] : Add Number Play List

--- lib/src/main/java/MathOperation/MathApp.java
+++ lib/src/main/java/MathOperation/MathApp.java
@@ -1,5 +1,10 @@
 package MathOperation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 @FunctionalInterface
 interface ICalculator{
 	public int calculate(int a, int b);
@@ -31,6 +36,37 @@ public class MathApp {
 		ICalculator.printResult(10, 8, "Subtraction", subtract);
 		ICalculator.printResult(9, 0, "Divison", divide);
 		ICalculator.printResult(12, 3, "Divison", divide);
+		
+		List<Integer> numberPlayList = new ArrayList<>();
+		System.out.println("Enter the size of the list:");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		for(int i= 0; i<size; i++)
+			numberPlayList.add(i+100);
+		
+		//Using Proper Class
+		class MyConsumer implements Consumer<Integer> {
+			public void accept(Integer t) {
+				System.out.println("Traversing with Consumer interface implementation, element in the number list : "+t);
+			}
+		}
+		MyConsumer myConsumer = new MyConsumer();
+		numberPlayList.forEach(myConsumer);
+		
+		//Using Anonymous Class
+		numberPlayList.forEach(new Consumer<Integer>() {
+			public void accept(Integer t) {
+				System.out.println("Using anonymous class, element in the number list : "+t);
+			}
+		});
+		//Using Explicit Lambda Function
+		Consumer<Integer> consumer = (n) -> {
+			System.out.println("Using explicit lambda function, element in the number list : "+n);
+		};
+		numberPlayList.forEach(consumer);
+		
+		//Using Implicit Lambda Function
+		numberPlayList.forEach((n) -> {System.out.println("Using implicit lambda function, element in the number list : "+n);});
 		
 	}
 
