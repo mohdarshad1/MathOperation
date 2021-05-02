c47c636a547284931d94f23c513c0dbc3d9f19e7
mohdarshad1 <mdarshad181@gmail.com>
2021-05-02 18:06:58 +0530

    [Mohd Arshad] : Add Ability to shoe result of Math Opertion

--- lib/src/main/java/MathOperation/MathApp.java
+++ lib/src/main/java/MathOperation/MathApp.java
@@ -3,8 +3,10 @@ package MathOperation;
 @FunctionalInterface
 interface ICalculator{
 	public int calculate(int a, int b);
+	static void printResult(int x, int y, String method, ICalculator iCalculator) {
+		System.out.println(method+": ("+x+","+y+") = "+iCalculator.calculate(x, y));
+	}
 }
-
 public class MathApp {
 
 	public static void main(String[] args) {
@@ -24,5 +26,12 @@ public class MathApp {
 		System.out.println("Subtraction of 8 from 10 is "+subtract.calculate(10,8));
 		System.out.println("Divison of 9 by 0 is "+divide.calculate(9,0));
 		System.out.println("Divison of 12 by 3 is "+divide.calculate(12,3));
+		
+		ICalculator.printResult(6, 5, "Addition",add);
+		ICalculator.printResult(10, 8, "Subtraction", subtract);
+		ICalculator.printResult(9, 0, "Divison", divide);
+		ICalculator.printResult(12, 3, "Divison", divide);
+		
 	}
+
 }
